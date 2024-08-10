b8b5e0a235aa543626ab79ce4feb00ffcb859d32
Sachin / <sreesachin11226@gmail.com>
2024-08-10 06:25:40 +0530

    feat: express basic boilerplate + env

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from "cors"
+import { configDotenv } from "dotenv";
+configDotenv()
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(PORT, () => console.info(`Server started on port ${PORT}!`))
